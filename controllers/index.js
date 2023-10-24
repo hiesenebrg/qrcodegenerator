364f0bc431d279ce05d7823fa763d9bdf1d02c45
hiesenebrg <adarshsingh8008@gmail.com>
2023-10-24 13:06:50 +0530

    pdf generator added

--- controllers/index.js
+++ controllers/index.js
@@ -1,37 +1,185 @@
 const QRCode = require("qrcode");
 const outputPath = "uploads";
-const fs = require('fs');
-const { error } = require("console");
-if (!fs.existsSync(outputPath)) {
-    fs.mkdirSync(outputPath);
-  }
-  const currentDirectory = process.cwd();
-  console.log(``)
-module.exports.qrgenerator = async function (req, res) {
-  try {
-    let vCardData = `BEGIN:VCARD\nVERSION:3.0\nFN:${req.body.name}\nTEL:${req.body.mobile}\nORG:${req.body.org}\nTITLE:${req.body.title}\nEMAIL;TYPE=work:${req.body.email}\nEND:VCARD`;
+const fs = require("fs");
+const currentDirectory = process.cwd();
+const puppeteer = require("puppeteer");
 
-    QRCode.toFile(`${currentDirectory}/uploads/${req.body.name}.png`, vCardData, (err) => {
+// Function to read an image file and return it as a base64 string
+const getImageAsBase64 = (filePath) => {
+  try {
+    const data = fs.readFileSync(filePath);
+    return `data:image/png;base64,${data.toString("base64")}`;
+  } catch (error) {
+    console.error("Error reading image:", error);
+    return null;
+  }
+};
+
+module.exports.qrgenerator = async (req, res) => {
+  console.log("api hit");
+  const originalName = req.body.name;
+  const nameWithoutSpaces = originalName.replace(/\s+/g, "");
+  let vCardData = `BEGIN:VCARD\nVERSION:3.0\nN:${req.body.name}\nTEL:${req.body.mobile}\nORG:${req.body.org}\nTITLE:${req.body.title}\nEMAIL;TYPE=work:${req.body.email}\nEND:VCARD`;
+
+  try {
+    QRCode.toFile(`${currentDirectory}/uploads/${nameWithoutSpaces}.png`, vCardData, async (err) => {
       if (err) {
-        console.log(error)
         res.status(500).send({
           message: "Internal server Errors!",
           error: err,
         });
       } else {
-        const originalName = req.body.name; // The name with spaces
-const nameWithoutSpaces = originalName.replace(/\s+/g, '');
-        res.status(200).send({
-          message: "Hurray!QR Code geneerated!",
-          data: `https://qrcodegenrator.onrender.com/${outputPath}/${nameWithoutSpaces}.png`,
-        });
+        try {
+          const qrCodePath = `${currentDirectory}/uploads/${nameWithoutSpaces}.png`;
+
+          // Read images as base64
+          const qrCodeBase64 = getImageAsBase64(qrCodePath);
+          const companyLogoBase64 = getImageAsBase64(`${currentDirectory}/uploads/MindSprint_Logo.jpg`);
+
+          if (!qrCodeBase64 || !companyLogoBase64) {
+            throw new Error("Error reading image files as base64");
+          }
+
+          // Load the HTML template with base64-encoded images
+          const htmlContent = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <title>QR Code Styling</title>
+           
+            <style>
+            .container{
+              height: 32vh;
+              width:80vw;
+              margin:auto;
+              padding:10px;
+            }
+            .border{
+              height: 96%;
+              width: 96%;
+              margin:auto;
+              padding:10px;
+              display: flex;
+              flex-direction: column;
+              border: 2px solid red;
+              border-radius: 6px;
+            }
+            .heading{
+              height: 30%;
+              width:96%;
+            }
+            #logo{
+              height: 100%;
+              width: 100%;
+            }
+            .info{
+              height: 96%;
+              width: 96%;
+              padding:20px;
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              margin-top:-15px;
+            
+            }.details{
+              height: 96%;
+              width: 46%;
+              padding:10px;
+              display: flex;
+              flex-direction: column;
+              align-items: flex-start;
+              justify-content: center;
+              
+              
+            
+            }.iconmanager{
+              height: 15%;
+              width: 15%;
+              display: flex;
+              flex-direction: row;
+              justify-content: space-between;
+              align-items: center;
+              font-size:1.5rem;
+            }
+            .iconmanager img{
+              height: 100%;
+              width: 100%;
+            }
+            .name{
+              font-size: 2.5rem;
+            }
+            </style>
+          </head>
+          <body>
+          <div class="container">
+          <div class="border">
+            <div class="heading">
+              <img id="logo" src="${companyLogoBase64}" alt="MindSprint Logo" />
+            </div>
+            <div class="info">
+              <div class="details">
+                <div class="name">
+                  ${req.body.name}
+                </div>
+               ${req.body.title}
+               <br>
+               <br>
+                <div  class="iconmanager">
+                  <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png">
+                  &nbsp;${req.body.mobile}
+              </div>
+                <div  class="iconmanager">
+                  <img src="https://logowik.com/content/uploads/images/513_email.jpg">
+                  &nbsp;${req.body.email}
+                </div>
+              </div>
+              <div class="qrcode">
+                <img src="${qrCodeBase64}" alt="qr-code" />
+              </div>
+            </div>
+          </div>
+        </div>
+  
+        
+          </body>
+          </html>
+          
+`;
+
+          // Create a Puppeteer browser instance
+          const browser = await puppeteer.launch();
+          const page = await browser.newPage();
+
+          // Navigate to an HTML page
+          await page.setContent(htmlContent);
+
+          // Generate a PDF from the HTML
+          const pdfBuffer = await page.pdf({ format: 'A4' }); // You can customize the format as needed
+
+          // Close the browser
+          await browser.close();
+
+          // Set the response headers for a PDF file
+          res.setHeader("Content-Type", "application/pdf");
+          res.setHeader("Content-Disposition", "attachment; filename=digital_card.pdf");
+
+          // Send the PDF buffer as the response
+          return res.status(200).send(pdfBuffer);
+        } catch (error) {
+          console.log("error in styling: ", error);
+          return res.status(500).send({
+            message: "Error generating PDF",
+            error: error,
+          });
+        }
       }
     });
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({
-      message: "Internal server Error!",
-      error: error,
+  } catch (pdfError) {
+    console.log("the error is ", pdfError);
+    return res.status(500).send({
+      message: "Error generating PDF image",
+      error: pdfError,
     });
   }
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 const qrcontroller = require("../../controllers");
 router.post("/generateqr", qrcontroller.qrgenerator);
-
+// router.post("/generateqr", function(req, res){
+//     return qrcontroller.qrgenerator
+//   });
 module.exports = router;
